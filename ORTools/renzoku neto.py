5940c20b2b634a25e88c4a3cd6bf27538925d55f
Sutams <48838483+Sutams@users.noreply.github.com>
2023-05-21 22:13:03 -0400

    Renzoku solver attempt #1

---
+++ ORTools/renzoku neto.py
@@ -0,0 +1,97 @@
+from ortools.sat.python import cp_model as cp
+import numpy as np
+
+
+class VarArraySolutionPrinter(cp.CpSolverSolutionCallback):
+    """Print intermediate solutions."""
+
+    def __init__(self, variables):
+        cp.CpSolverSolutionCallback.__init__(self)
+        self.__variables = variables
+        self.__solution_count = 0
+
+    def on_solution_callback(self):
+        self.__solution_count += 1
+        for row in self.__variables:
+            for v in row:
+                print('%i' % (self.Value(v)), end=' ')
+            print()
+        print()
+            
+    
+    def solution_count(self):
+        return self.__solution_count
+
+
+dotsX = [(0, 0, 0),
+         (0, 0, 1),
+         (1, 0, 0),
+         (0, 1, 0)]
+
+dotsY = [(0, 0, 0, 1),
+         (1, 1, 1, 1),
+         (0, 1, 0, 0)]
+
+grid = [(0, 4, 0, 0),
+        (0, 0, 0, 0),
+        (0, 0, 0, 0),
+        (0, 0, 0, 0)]
+
+
+def solve_rensoku(n, dX, dY, grid=None):
+    model = cp.CpModel()
+    tmp = model.NewIntVar(1, 1, '')
+    
+    if grid is None: grid = np.zeros([n, n])
+    mtrx = [[model.NewIntVar(grid[i][j], grid[i][j], 'x({},{})'.format(i, j))
+             if grid[i][j] else model.NewIntVar(1, n, 'x({},{})'.format(i, j))
+             for j in range(n)] for i in range(n)]
+
+    # constraints
+    for i, row in enumerate(mtrx):
+        model.AddAllDifferent(row)
+        for j, x in enumerate(row[0:len(row)-1]):
+            if dX[i][j] == 1:
+                tmpbool = model.NewBoolVar('')
+                model.Add(x == row[j+1] + tmp).OnlyEnforceIf(tmpbool)
+                model.Add(x == row[j+1] - tmp).OnlyEnforceIf(tmpbool.Not())
+            else:
+                tmpbool = model.NewBoolVar('')
+                model.Add(x != row[j+1] + tmp).OnlyEnforceIf(tmpbool)
+                model.Add(x != row[j+1] - tmp).OnlyEnforceIf(tmpbool.Not())
+
+    mtrx = np.transpose(mtrx)
+    for i, row in enumerate(mtrx):
+        model.AddAllDifferent(row)
+        for j, x in enumerate(row[0:len(row)-1]):
+            if dY[i][j] == 1:
+                tmpbool = model.NewBoolVar('')
+                model.Add(x == row[j+1] + tmp).OnlyEnforceIf(tmpbool)
+                model.Add(x == row[j+1] - tmp).OnlyEnforceIf(tmpbool.Not())
+            else:
+                tmpbool = model.NewBoolVar('')
+                model.Add(x != row[j+1] + tmp).OnlyEnforceIf(tmpbool)
+                model.Add(x != row[j+1] - tmp).OnlyEnforceIf(tmpbool.Not())
+    mtrx = np.transpose(mtrx)
+    
+    # Solver
+    solver = cp.CpSolver()
+    solution_printer = VarArraySolutionPrinter(mtrx)
+    solver.parameters.enumerate_all_solutions = True
+    status = solver.Solve(model, solution_printer)
+    print('Status = %s' % solver.StatusName(status))
+    print('Number of solutions found: %i' % solution_printer.solution_count())
+
+solve_rensoku(len(dotsX), dotsX, np.transpose(dotsY), grid)
+
+#    0 1 2 3
+                  # dX
+# 0  2 4 1 3     (0, 0, 0)
+# 1  4 1 3 2     (0, 0, 1)
+# 2  3 2 4 1     (1, 0, 0)
+# 3  1 3 2 4     (0, 1, 0)
+                            # dY
+# 0  2 4 1 3     0 1 0     (0, 0, 0, 1)
+# 1  4 1 3 2     0 1 1     (1, 1, 1, 1)
+# 2  3 2 4 1     0 1 0     (0, 1, 0, 0)
+# 3  1 3 2 4     1 1 0
