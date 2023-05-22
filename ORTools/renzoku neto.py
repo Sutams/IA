165ccf73a140116a3a59148757baf53ac617d844
Sutams <48838483+Sutams@users.noreply.github.com>
2023-05-22 12:11:33 -0400

    Update renzoku neto.py
    Added comments

--- ORTools/renzoku neto.py
+++ ORTools/renzoku neto.py
@@ -1,7 +1,7 @@
 from ortools.sat.python import cp_model as cp
 import numpy as np
 
-
+# standard solution printer modified to print a matrix
 class VarArraySolutionPrinter(cp.CpSolverSolutionCallback):
     """Print intermediate solutions."""
 
@@ -18,11 +18,10 @@ class VarArraySolutionPrinter(cp.CpSolverSolutionCallback):
             print()
         print()
             
-    
     def solution_count(self):
         return self.__solution_count
 
-
+# dots that symbol two cells are consecutive, x for rows, y for cols
 dotsX = [(0, 0, 0),
          (0, 0, 1),
          (1, 0, 0),
@@ -37,17 +36,19 @@ grid = [(0, 4, 0, 0),
         (0, 0, 0, 0),
         (0, 0, 0, 0)]
 
-
-def solve_rensoku(n, dX, dY, grid=None):
+def solve_renzoku(n, dX, dY, grid=None):
     model = cp.CpModel()
+    
     tmp = model.NewIntVar(1, 1, '')
     
     if grid is None: grid = np.zeros([n, n])
+    
+    # keeps grid values and else makes NewIntVars
     mtrx = [[model.NewIntVar(grid[i][j], grid[i][j], 'x({},{})'.format(i, j))
              if grid[i][j] else model.NewIntVar(1, n, 'x({},{})'.format(i, j))
              for j in range(n)] for i in range(n)]
 
-    # constraints
+    # row constraints
     for i, row in enumerate(mtrx):
         model.AddAllDifferent(row)
         for j, x in enumerate(row[0:len(row)-1]):
@@ -59,7 +60,8 @@ def solve_rensoku(n, dX, dY, grid=None):
                 tmpbool = model.NewBoolVar('')
                 model.Add(x != row[j+1] + tmp).OnlyEnforceIf(tmpbool)
                 model.Add(x != row[j+1] - tmp).OnlyEnforceIf(tmpbool.Not())
-
+    
+    # col constraints
     mtrx = np.transpose(mtrx)
     for i, row in enumerate(mtrx):
         model.AddAllDifferent(row)
@@ -72,9 +74,11 @@ def solve_rensoku(n, dX, dY, grid=None):
                 tmpbool = model.NewBoolVar('')
                 model.Add(x != row[j+1] + tmp).OnlyEnforceIf(tmpbool)
                 model.Add(x != row[j+1] - tmp).OnlyEnforceIf(tmpbool.Not())
+    
+    # transpose back to normal
     mtrx = np.transpose(mtrx)
     
-    # Solver
+    # solve and print
     solver = cp.CpSolver()
     solution_printer = VarArraySolutionPrinter(mtrx)
     solver.parameters.enumerate_all_solutions = True
@@ -82,9 +86,9 @@ def solve_rensoku(n, dX, dY, grid=None):
     print('Status = %s' % solver.StatusName(status))
     print('Number of solutions found: %i' % solution_printer.solution_count())
 
-solve_rensoku(len(dotsX), dotsX, np.transpose(dotsY), grid)
+solve_renzoku(len(dotsX), dotsX, np.transpose(dotsY), grid)
 
-#    0 1 2 3
+#idx 0 1 2 3
                   # dX
 # 0  2 4 1 3     (0, 0, 0)
 # 1  4 1 3 2     (0, 0, 1)
